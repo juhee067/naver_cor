a86613ed0389e546d7c4b2fb157cf9a633087bc6
juhee <wngmlchoi@naver.com>
2022-10-20 22:46:31 +0900

    20221020

--- src/component/MainVisual.js
+++ src/component/MainVisual.js
@@ -1,8 +1,18 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
 const MainVisual = () => {
   return (
     <div className="MainSlide">
       <Slider arrows={false} autoplay={true} pauseOnHover={false}>
-        <figure></figure>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
       </Slider>
     </div>
   );
