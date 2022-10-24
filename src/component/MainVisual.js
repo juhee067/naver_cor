8cbec22ab8c9571435a3a6a8b9bc9cef28664d5c
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-10-24 17:36:25 +0900

    20221024

--- src/component/MainVisual.js
+++ src/component/MainVisual.js
@@ -1,23 +1,27 @@
-import React from "react";
+import { useEffect, useRef, useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "../css/MainVisual.scss";
 import { mainContent } from "../data/common";
 
 const MainVisual = () => {
-
+  const [IDX, setIDX] = useState();
+  useEffect(() => {
+    setIDX(0)
+  }, []);
+  const mainSlide = useRef(null);
   const settings = {
     arrows: false,
     autoplay: true,
     pauseOnHover: false,
     dots: true,
-
+    afterChange: index => setIDX(index),
 
   };
 
   return (
     <div className="MainVisual">
-      <Slider {...settings}>
+      <Slider {...settings} ref={mainSlide}>
         {mainContent.map((slide, idx) => (
           <div className="mainSlider" key={idx}>
             <figure className={`itm itm0${idx + 1}`} key={idx}>
@@ -26,15 +30,17 @@ const MainVisual = () => {
                 <div className="inner">
                   <strong>{slide.strong}</strong>
                   <p>{slide.content}</p>
+                  <div className="slideNum">
+                    0{IDX + 1}<span> / 0{mainContent.length}</span>
+                  </div>
                 </div>
 
               </div>
               <div className="allows">
-                <i className="xi-angle-left left"></i>
-                <i className="xi-angle-right right"></i>
-
-
+                <i onClick={() => mainSlide.current.slickPrev()} className='xi-arrow-left left'></i>
+                <i onClick={() => mainSlide.current.slickNext()} className='xi-arrow-right right'></i>
               </div>
+
             </figure>
 
           </div>
