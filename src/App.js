faf267739a5aaa2921c2c1c17112ca1f5032086a
juhee <wngmlchoi@naver.com>
2022-10-26 17:17:58 +0900

    20221026

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./component/Header";
 import MainVisual from "./component/MainVisual";
 import Notice from "./component/Notice";
+import Platform from "./component/Platform";
 import Service from "./component/Service";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <MainVisual />
       <Notice />
       <Service />
+      <Platform />
     </>
   );
 }
--- src/component/Platform.js
+++ src/component/Platform.js
@@ -1,19 +1,32 @@
 import "../css/Platform.scss";
-
+import { platform } from "../data/common";
 const Platform = () => {
   return (
     <section className="Platform">
-      <div className="title">
-        <h2> 기술로 다양성을 꽃피우는 플랫폼</h2>
-        <p>
-          {" "}
-          <span>
-            개인의 다양한 가능성이 의미있는 성공으로 꽃 피울 수 있도록 네이버가
-          </span>
-          서비스와 기술플랫폼을 통해 더 가까이 서포트(SUPPORT) 합니다.
-        </p>
+      <div className="container">
+        {" "}
+        <div className="title">
+          <h2> 기술로 다양성을 꽃피우는 플랫폼</h2>
+          <p>
+            {" "}
+            <span>
+              개인의 다양한 가능성이 의미있는 성공으로 꽃 피울 수 있도록
+            </span>
+            네이버가 서비스와 기술플랫폼을 통해 더 가까이 서포트(SUPPORT)
+            합니다.
+          </p>
+        </div>
+        <div className="content">
+          {platform.map((platform, idx) => (
+            <div className={`form from0${idx + 1}`} key={idx}>
+              <a href="/" className={`pic0${idx + 1}`} key={idx}>
+                {platform.title}
+              </a>
+              <p>{platform.des}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="content"></div>
     </section>
   );
 };
